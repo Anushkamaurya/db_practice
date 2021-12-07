661229c6181523fd8952fa012a012b3768bf5722
tejashribhargav <tejashribhargav@gmail.com>
2021-12-07 14:43:00 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -5,6 +5,6 @@ public class Sum {
       int num1 = 5, num2 = 15, sum, num3 = 10, num4 = 16;
       sum = num1 + num2 + num3 + num4;
 
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sum of these  numbers: "+sum);
    }
 }
