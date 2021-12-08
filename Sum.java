370e966f8273fe70134eba4e5a3eb879497a90f2
Anushkamaurya <94072914+Anushkamaurya@users.noreply.github.com>
2021-12-08 14:37:43 +0530

    Commit successfully

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum, num3 = 10, num4 = 16;
+      int num1 = 5, num2 = 15, sum, num3 = 10, num4 = 15;
       sum = num1 + num2 + num3 + num4;
 
       System.out.println("Sum of these  numbers: "+sum);
