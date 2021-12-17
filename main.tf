88a7217c7d281ba6f240ff196c7f35c8972e0bdb
Anushkamaurya <94072914+Anushkamaurya@users.noreply.github.com>
2021-12-17 18:04:02 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,43 +1,42 @@
-resource "google_service_account" "default" {
-  account_id   = "service_account_id"
-  display_name = "Service Account"
-}
-
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
     }
   }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
 }
+
+provider "google" {
+  project = "mylearning-331213"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
+}
+
+resource "google_compute_network" "t_vpc_network" {
+  name                    = "terra_network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "t_subnet" {
+  name          = "terra-subnetwork"
+  ip_cidr_range = "192.168.0.0/20"
+  region        = "asia-south1"
+  network       = google_compute_network.t_vpc_network.id
+  depends_on    = ["google_compute_network.t_vpc_network"]
+ 
+}
+
+resource "google_compute_firewall" "t_firewall" {
+  name    = "terra-firewall"
+  network = google_compute_network.t_vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
